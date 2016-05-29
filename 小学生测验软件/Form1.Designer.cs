b6a3b9ec77891e27f1d4187951ce5dcec59cdb60
1209079611 <hjd120907961111@gmail.com>
2016-05-29 15:13:07 +0800

    V1.1第一次提交

---
+++ 小学生测验软件/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace 小学生测验软件
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("宋体", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(39, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 65);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1+1=";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(36, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "当前分数：0";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(36, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(190, 22);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "第一题";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(308, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.WordWrap = false;
+        //    this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(308, 132);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 32);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "提交";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(36, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(222, 31);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "本题剩余时间：";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(308, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 34);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "查看错题";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(24, 27);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(251, 228);
+            this.listBox1.TabIndex = 8;
+            this.listBox1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 282);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "小学生测验软件";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ 小学生测验软件/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 小学生测验软件
+{
+    public partial class Form1 : Form
+    {
+        int question_num = 1;     //题号
+        double score = 0;          //分数
+        //int sum;
+        //int chance = 3;        //每道题有三次机会
+        int t = 60;
+        save_formula save_f = new save_formula();
+        //int sum;
+        class save_formula      //保存类，纪录每道题的算式和正确与否
+        {
+            public int[] frist_save = new int [10];
+            public int[] sec_save = new int[10];
+            public char[] operater = new char[10];
+            public int[] sum_save = new int[10];
+            public string[] ft = new string[10];
+            public int []chance = new int [10];
+        }
+
+
+
+        class Formula       //算式类，每道题都是该类的对象
+        {
+            public int first_number;
+            public int second_number;
+            public int sum_number;
+            public string operater;       //加减法运算符
+           
+
+            public Formula()        //构造函数
+            {
+
+            }
+            public void build_equation()        //生成符合要求的算式
+            {
+                Random ran = new Random();
+
+                first_number = ran.Next(0, 50);
+                second_number = ran.Next(0, 50);
+                int t = ran.Next(1, 10);
+                if (t % 2 == 0)
+                {
+                    operater = "+";
+                }
+                else
+                {
+                    operater = "-";
+                }
+                sum_number = t % 2 == 0 ? first_number + second_number : first_number - second_number;
+                //sum = sum_number;
+                //sum_number = first_number + second_number;
+            }
+        }
+
+        void show_formula()     //显示算式
+        {
+            Formula f1 = new Formula();
+            f1.build_equation();
+            while (f1.sum_number > 50 || f1.sum_number < 0)
+                f1.build_equation();
+            label1.Text = f1.first_number + f1.operater + f1.second_number + "=";
+            //textBox1.Text = Convert.ToString(f1.sum_number);
+            //sum = f1.sum_number;
+            save_f.frist_save[question_num - 1] = f1.first_number;
+            save_f.sec_save[question_num - 1] = f1.second_number;
+            save_f.operater[question_num - 1] = Convert.ToChar(f1.operater);
+            save_f.sum_save[question_num - 1] = f1.sum_number;
+            save_f.chance[question_num - 1] = 3;
+         //   Console.WriteLine("{0}{1}{2}={3}",save_f.frist_save,save_f.operater,save_f.sec_save,save_f.sum_save);
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label3.Text = "第" + question_num + "题" + "（共10题）";
+            show_formula();
+            timer1.Start();
+           // save_formula sf = new save_formula();
+        }
+
+        public void next()
+        {
+            if (question_num == 10)
+            {
+                result();
+                button2.Text = "重新测试";
+                button1.Visible = true;
+            }
+            else
+            {
+                
+                textBox1.Clear();
+              
+                question_num++;
+                save_f.chance[question_num-1]=3;     //重置三次机会
+                show_formula();
+                label3.Text = "第" + question_num + "题" + "（共10题）";
+                button2.Text = "提交";
+                textBox1.Focus();
+                
+                t = -5*question_num+ 65;        //时间随题号变化
+
+            }
+        }
+
+        public void result()
+        {
+            if (score == 100)
+                MessageBox.Show("你真是个天才", "温馨提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+            {
+                if (score >= 90 && score <= 99)
+                    MessageBox.Show("不错哟", "温馨提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else if (score >= 80 && score <= 89)
+                    MessageBox.Show("非常棒", "温馨提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else if (score >= 70 && score <= 79)
+                    MessageBox.Show("还需努力哟", "温馨提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else if (score >= 60 && score <= 69)
+                    MessageBox.Show("要加油啦", "温馨提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else if (score < 60)
+                    MessageBox.Show("很遗憾，再来一次", "温馨提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "提交")
+            {
+                if (textBox1.Text == "")
+                    MessageBox.Show("请输入你的答案~~~", "错误提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                else
+                    if (Convert.ToInt32(textBox1.Text) == save_f.sum_save[question_num - 1])
+                {
+                    double temp = 0.5 * save_f.chance[question_num-1] * save_f.chance[question_num-1] + 0.5 * save_f.chance[question_num-1] + 4;
+                    score += temp;
+                    MessageBox.Show("回答正确", "温馨提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    label2.Text = "当前分数：" + score;
+                    button2.Text = "下一题";
+                    next();
+                  //  save_f.ft[question_num-1] = "正确";
+                    timer1.Start();
+
+                }
+                else
+                {
+                    save_f.chance[question_num-1]--;
+                    if(save_f.chance[question_num-1] == 0)
+                    {
+                        MessageBox.Show("回答错误，本题答案是" + save_f.sum_save[question_num - 1] , "提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("输入错误，请重新输入，本题你还有" + save_f.chance[question_num-1] + "次机会。", "错误提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    }
+                    textBox1.Focus();
+                  //  save_f.ft[question_num-1] ="错误";
+                }
+                if (save_f.chance[question_num-1] < 1)
+                {
+                 //   button2.Text = "下一题";
+                    next();
+                 //   save_f.ft[question_num-1] = "错误";
+
+                }
+
+                
+
+
+            }
+            else if (button2.Text == "重新测试")
+            {
+                if (MessageBox.Show("要重新测试吗?", "提示", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                {
+                    question_num = 1;     //题号
+                    score = 0;          //分数
+                    for (int i = 0; i < 10; i++)
+                    {
+                        save_f.chance[i] = 3;
+                    }
+                    t = 60;
+                    //save_formula save_f = new save_formula();
+                    label3.Text = "第" + question_num + "题" + "（共10题）";
+                    label2.Text = "当前分数：" + score;
+                    show_formula();
+                    timer1.Start();
+                    button2.Text = "提交";
+                    listBox1.Visible = false;
+                    textBox1.Visible = true;
+                    button1.Visible = false;
+                    textBox1.Text = "";
+                    textBox1.Focus();
+                }
+              
+            }
+        }
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            label4.Text = "本题剩余时间："+Convert.ToString(t--);
+            if (t < 0)
+            {
+                timer1.Stop();
+                MessageBox.Show("本题时间到！进入下一题，按确定继续。");
+                next();
+                timer1.Start();
+                
+            }
+            else
+            {
+                if (question_num < 9)
+                {
+                    
+                }
+                else
+                {
+                    timer1.Stop();
+                }
+            }
+
+
+        }
+
+
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)  //按下enter，自动显示答案
+        {
+         //   Console.WriteLine("{0}", sum);
+            if (e.KeyChar == 13)
+                textBox1.Text = Convert.ToString(save_f.sum_save[question_num - 1]);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Visible = !listBox1.Visible;
+            textBox1.Visible = !textBox1.Visible;
+            listBox1.Items.Clear();
+            for(int i=0;i<10;i++)
+            {
+                if(save_f.chance[i]==3)
+                {
+                    save_f.ft[i] = "第一次答对";
+                }
+                if (save_f.chance[i] ==2)
+                {
+                    save_f.ft[i] = "第二次答对";
+                }
+                if (save_f.chance[i] == 1)
+                {
+                    save_f.ft[i] = "第三次答对";
+                }
+                if (save_f.chance[i] <= 0)
+                {
+                    save_f.ft[i] = "错误";
+                }
+            }
+            for(int i=0;i<10;i++)
+            {
+                listBox1.Items.Add("第"+Convert.ToString(i +1)+"题  "+Convert.ToString(save_f.frist_save[i]) + Convert.ToString(save_f.operater[i]) + Convert.ToString(save_f.sec_save[i]) + "=" + Convert.ToString(save_f.sum_save[i])+"   "+Convert.ToString(save_f.ft[i]));
+            }
+               // infor = Convert.ToString(save_f.frist_save[i]) + Convert.ToString(save_f.operater[i]) + Convert.ToString(save_f.sec_save[i]) + "=" + Convert.ToString(save_f.sum_save[i]) + "\n";
+        }
+    }
+}
